77a0865edf3d5a5a19f090042962f11cd4716311
Denis Skidan <skidan147@gmail.com>
2023-11-23 19:15:52 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
+
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(people, gender) {
+	if (people.length === 0) {
+		return 0;
+	}
+
+	if (gender !== "мужской" && gender !== "женский") {
+		return 0;
+	}
+
+	let result = people.filter((user) => user.gender === gender).reduce((acc, user) => acc + user.age, 0) / people.filter((user) => user.gender === gender).length;
+
+	return result;
 }
