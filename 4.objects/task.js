52df34520d9e118f758751ea9d81671e13ca411b
Denis Skidan <skidan147@gmail.com>
2023-11-30 18:26:30 +0300

    Четвёртое задание полностью готово!

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,56 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+	return this.subject;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		return;
+	}
+	this.marks.push(...marksToAdd);
+	return this.marks;
 }
 
-Student.prototype.getAverage = function () {
-  
+
+
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < this.marks.length; i++) {
+		sum += this.marks[i];
+	}
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+
+
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+	return this.excluded
 }
+
+let student1 = new Student("Vasilii", "mail", 29);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(5, 5, 5);
+console.log(student1.getAverage());
+console.log(student1);
+
+
+let student2 = new Student("Anna", "femail", 35);
+student2.setSubject("Geometry");
+student2.exclude("Bad marks")
+console.log(student2);
+  
